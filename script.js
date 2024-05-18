b51e666050e059515910e3c45679a122b369937c
Ahmad Asadi Looyeh <asadi.loyeh@gmail.com>
2024-05-18 20:32:31 +0330

    add my own dblclick event

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const image = document.querySelector(".image");
 let numberOfClicks = 0;
+let i = 0;
+let LastClick = 0;
 
-image.addEventListener("dblclick", (e) => {
-  console.log(e);
+function heart(e) {
   const div = document.createElement("div");
   div.className = "heart fas fa-heart";
   image.appendChild(div);
   div.style.top = `${e.offsetY}px`;
   div.style.left = `${e.offsetX}px`;
-  let i = 1;
+  i++;
+  document.querySelector(".like").querySelector("span").textContent = i;
   setTimeout(() => {
     div.remove();
   }, 400);
+}
+
+//adding heart on double click event
+// image.addEventListener("dblclick", (e) => {
+//   // console.log(e);
+//   heart(e);
+// });
+
+//adding heart on tow clicks event
+image.addEventListener("click", (e) => {
+  let timeNow = new Date().getTime();
+  if (timeNow - LastClick < 500) {
+    heart(e);
+  }
+  LastClick = timeNow;
 });
